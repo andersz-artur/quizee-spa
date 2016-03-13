d106dc458803cc023797284e072039585b6d8439
Artur Andersz <artur.andersz@netguru.pl>
2016-03-13 15:57:17 +0100

    actions for navigation

--- app/components/question-box.js
+++ app/components/question-box.js
@@ -1,8 +1,17 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  currentQuestion: Ember.computed('questions.@each', function() {
+  currentQuestionId: 1,
+  currentQuestion: Ember.computed('currentQuestionId', function() {
     let questions = this.get('questions');
-    return questions.findBy('id', 1);
-  })
+    return questions.findBy('id', this.get('currentQuestionId'));
+  }),
+  actions: {
+    nextQuestion() {
+      this.incrementProperty('currentQuestionId');
+    },
+    previousQuestion() {
+      this.decrementProperty('currentQuestionId');
+    }
+  }
 });
