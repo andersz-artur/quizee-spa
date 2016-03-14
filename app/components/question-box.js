058328350989983c6c461e2095e7c7bf8c3c4959
Artur Andersz <artur.andersz@netguru.pl>
2016-03-14 23:04:09 +0100

    select user answer

--- app/components/question-box.js
+++ app/components/question-box.js
@@ -2,12 +2,13 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   currentQuestionId: 1,
+  selectedAnswerId: 0,
   currentQuestion: Ember.computed('currentQuestionId', function() {
     let questions = this.get('questions');
     return questions.findBy('id', this.get('currentQuestionId'));
   }),
   didRender() {
-   this.$().hide().fadeIn(800);
+  //  this.$().hide().fadeIn(800);
   },
   actions: {
     nextQuestion() {
@@ -19,6 +20,9 @@ export default Ember.Component.extend({
       if (this.get('currentQuestionId') > 1) {
         this.decrementProperty('currentQuestionId');
       }
+    },
+    selectAnswer() {
+      this.set('selectedAnswerId', parseInt(Ember.$(window.event.target).prop('id'), 10));
     }
   }
 });
---
+++ app/helpers/equal.js
@@ -0,0 +1,10 @@
+export default Ember.Helper.helper(function(params) {
+  if (arguments.length < 2) {
+    throw new Error("Handlebars Helper equal needs 2 parameters");
+  }
+
+  let arg1 = params[0];
+  let arg2 = params[1];
+
+  return arg1===arg2
+});
